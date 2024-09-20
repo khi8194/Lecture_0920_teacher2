de0baeefc2341121957710c53761b384abb560d6
tubi55 <tubi55@nate.com>
2024-09-20 16:30:11 +0900

    margin속성을 absolute의 left로 변경처리하고 css와 js에서 %로 모션 이동 처리

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-//0 : margin-left: 0px; (-400 * 0)
-//1 : margin-left: -400px; (-400 * 1)
-//2 : margin-left: -800px; (-400 * 2)
-//3 : margin-left: -1200px; (-400 * 3)
-//4 : margin-left: -1600px; (-400 * 4)
+//0 : margin-left: 0px; (-400 * 0) :0%
+//1 : margin-left: -400px; (-400 * 1) : -100%
+//2 : margin-left: -800px; (-400 * 2) : -200%
+//3 : margin-left: -1200px; (-400 * 3) : -300%
+//4 : margin-left: -1600px; (-400 * 4) : -400%
 
 const frame = document.querySelector("main");
 const panel = frame.querySelector(".panel");
@@ -11,8 +11,9 @@ const btns = frame.querySelectorAll(".btns li");
 btns.forEach((btn, idx) => {
   btn.addEventListener("click", () => {
     console.log(idx);
-    new Anime(panel, { marginLeft: -400 * idx }, { duration: 500 });
+    new Anime(panel, { left: -100 * idx + "%" }, { duration: 500 });
   });
 });
 
-//미션 - 위의 코드에서 각각의 버튼을 클릭하면 해당 순번의 패널이 프레임 안쪽으로 들어오도록 new Anime를 활용해 좌우 슬라이더 모션 처리 (3시 50분까지)
+//미션 -  HTML에서 li의 갯수만 수정하면 scss에서 일일이 panel, li의 넓이를 수정할 필요가 없도록 스크립트에서 동적 처리
+//4시 40분까지
